9e0a0383d5e3519bbf8cdddb97b98f00dd0652b9
Suleyman Farajli <suleyman@farajli.net>
2025-01-08 20:28:49 +0400

    config.h: font changed

--- config.h
+++ config.h
@@ -7,10 +7,8 @@ static int min_width = 700;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 static unsigned int border_width = 0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = { "Liberation Mono:size=10" };
+static const char *prompt  = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
